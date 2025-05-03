5d50cdd3a87e0180753cac6ff067dd14009fb318
WajidAli05 <wajidalise@gmail.com>
2025-05-03 08:54:04 -0700

    Add product form common fields complete

--- Frontend/src/pages/AddProductPage.jsx
+++ Frontend/src/pages/AddProductPage.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import DropDownMenu from '../components/DropDownMenu'
-import NavBar from '../components/NavBar'
+import React from 'react';
+import DropDownMenu from '../components/DropDownMenu';
+import NavBar from '../components/NavBar';
+import { PlusOutlined } from '@ant-design/icons';
 import {
     Button,
     Cascader,
@@ -12,18 +13,29 @@ import {
     Segmented,
     Select,
     TreeSelect,
-  } from 'antd';
-  const { RangePicker } = DatePicker;
-  const formItemLayout = {
+    Upload,
+    Radio
+} from 'antd';
+const { RangePicker } = DatePicker;
+
+const formItemLayout = {
     labelCol: {
-      xs: { span: 24 },
-      sm: { span: 6 },
+        xs: { span: 24 },
+        sm: { span: 6 },
     },
     wrapperCol: {
-      xs: { span: 24 },
-      sm: { span: 14 },
+        xs: { span: 24 },
+        sm: { span: 14 },
     },
-  };
+};
+
+// normFile function to handle the file upload event
+const normFile = (e) => {
+    if (Array.isArray(e)) {
+        return e;
+    }
+    return e?.fileList;
+};
 
 const AddProductPage = () => {
     const categories = [
@@ -47,97 +59,157 @@ const AddProductPage = () => {
         { label: 'Baby Products', key: '18' },
         { label: 'Jewelry', key: '19' },
         { label: 'Shoes', key: '20' }
-    ]
+    ];
+
+    const fileTypes = [
+        { label: 'PDF', key: 'pdf' },
+        { label: 'ZIP', key: 'zip' },
+        { label: 'MP4', key: 'mp4' },
+        { label: 'MP3', key: 'mp3' },
+        { label: 'JPG', key: 'jpg' },
+        { label: 'PNG', key: 'png' },
+        { label: 'GIF', key: 'gif' },
+        { label: 'DOCX', key: 'docx' },
+        { label: 'TXT', key: 'txt' },
+        { label: 'EPUB', key: 'epub' },
+        { label: 'EXE', key: 'exe' }
+      ];      
+
     const [form] = Form.useForm();
-    const variant = Form.useWatch('variant', form);
 
-  return (
-    <div>
-        <NavBar />
-        <Form
-            {...formItemLayout}
-            form={form}
-            variant={'outlined'}
-            style={{ maxWidth: 600 }}
-            initialValues={{ variant: 'filled' }}
+    // to modify the form based on product type selected
+    const [productType, setProductType] = React.useState('physical');
+
+    return (
+        <div>
+            <NavBar />
+            <Form
+                {...formItemLayout}
+                form={form}
+                variant={'outlined'}
+                className='add-product-form'
+                initialValues={{ variant: 'filled' }}
             >
+                {/* Drop down to select type of product like physical, digital or affiliate */}
+                <Form.Item
+                    className='form-item'
+                    label="Product Type"
+                    name="variant"
+                    rules={[{ required: true, message: 'Select a type!' }]}
+                >
+                    <Segmented
+                        options={[
+                            { label: 'Physical', value: 'physical' },
+                            { label: 'Digital', value: 'digital' },
+                            { label: 'Affiliate', value: 'affiliate' },
+                        ]}
+                        value={productType}
+                        onChange={(value)=> setProductType(value)}
+                    />
+                </Form.Item>
+                {console.log(productType)}
 
-            <Form.Item label="Input" name="Input" rules={[{ required: true, message: 'Please input!' }]}>
-                <Input />
-            </Form.Item>
+                <Form.Item
+                    className='form-item'
+                    label="Category"
+                    name="Select"
+                    rules={[{ required: true, message: 'No category selected!' }]}
+                >
+                    <Select placeholder="Select a category" allowClear>
+                        {categories.map((category) => (
+                            <Select.Option key={category.key} value={category.key}>
+                                {category.label}
+                            </Select.Option>
+                        ))}
+                    </Select>
+                </Form.Item>
 
-            <Form.Item
-                label="InputNumber"
-                name="InputNumber"
-                rules={[{ required: true, message: 'Please input!' }]}
-            >
-                <InputNumber style={{ width: '100%' }} />
-            </Form.Item>
+                <Form.Item className='form-item' label="Product Name" name="Input" rules={[{ required: true, message: 'Product Name is empty!' }]}>
+                    <Input />
+                </Form.Item>
 
-            <Form.Item
-                label="TextArea"
-                name="TextArea"
-                rules={[{ required: true, message: 'Please input!' }]}
-            >
-                <Input.TextArea />
-            </Form.Item>
+                <Form.Item className='form-item' label="SKU" name="Input" rules={[{ required: true, message: 'Please input!' }]}>
+                    <Input />
+                </Form.Item>
 
-            <Form.Item
-                label="Mentions"
-                name="Mentions"
-                rules={[{ required: true, message: 'Please input!' }]}
-            >
-                <Mentions />
-            </Form.Item>
+                <Form.Item
+                    className='form-item'
+                    label="Price"
+                    name="InputNumber"
+                    rules={[{ required: true, message: 'Price is empty!' }]}
+                >
+                    <InputNumber style={{ width: '100%' }} />
+                </Form.Item>
 
-            <Form.Item
-                label="Select"
-                name="Select"
-                rules={[{ required: true, message: 'Please input!' }]}
-            >
-                <Select />
-            </Form.Item>
+                <Form.Item
+                    className='form-item'
+                    label="Stock"
+                    name="InputNumber"
+                    rules={[{ required: true, message: 'Stock is empty!' }]}
+                >
+                    <InputNumber style={{ width: '100%' }} />
+                </Form.Item>
 
-            <Form.Item
-                label="Cascader"
-                name="Cascader"
-                rules={[{ required: true, message: 'Please input!' }]}
-            >
-                <Cascader />
-            </Form.Item>
+                <Form.Item className='form-item' label="Availibility" rules={[{ required: true, message: 'Select an option!' }]}>
+                    <Radio.Group>
+                        <Radio value="In Stock"> In Stock </Radio>
+                        <Radio value="Out of Stock"> Out of Stock </Radio>
+                    </Radio.Group>
+                </Form.Item>
 
-            <Form.Item
-                label="TreeSelect"
-                name="TreeSelect"
-                rules={[{ required: true, message: 'Please input!' }]}
-            >
-                <TreeSelect />
-            </Form.Item>
+                <Form.Item
+                    className='form-item'
+                    label="Description"
+                    name="TextArea"
+                    rules={[{ required: true, message: 'Please input!' }]}
+                >
+                    <Input.TextArea />
+                </Form.Item>
 
-            <Form.Item
-                label="DatePicker"
-                name="DatePicker"
-                rules={[{ required: true, message: 'Please input!' }]}
-            >
-                <DatePicker />
-            </Form.Item>
+                {productType === 'digital' || productType === 'affiliate' && (
+                    <Form.Item className='form-item' label="Link" name="Input" rules={[{ required: true, message: 'Link Name is empty!' }]}>
+                        <Input />
+                    </Form.Item>
+                )}
 
-            <Form.Item
-                label="RangePicker"
-                name="RangePicker"
-                rules={[{ required: true, message: 'Please input!' }]}
-            >
-                <RangePicker />
-            </Form.Item>
+                {productType === 'digital' && (
+                    <Form.Item
+                        className='form-item'
+                            label="File Type"
+                            name="Select"
+                            rules={[{ required: true, message: 'No File Type selected!' }]}
+                            >
+                                <Select placeholder="Select File Type" allowClear>
+                                    {fileTypes.map((fileType) => (
+                                        <Select.Option key={fileType.key} value={fileType.key}>
+                                            {fileType.label}
+                                        </Select.Option>
+                                            ))}
+                                </Select>
+                    </Form.Item>
+                )}
 
-            <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
-                <Button type="primary" htmlType="submit">
-                Submit
-                </Button>
-            </Form.Item>
-        </Form>
-    </div>
-  )
-}
+                {/* File Upload Section */}
+                <Form.Item className='form-item' label="Upload" valuePropName="fileList" getValueFromEvent={normFile}>
+                    <Upload action="/upload.do" listType="picture-card">
+                        <button
+                            style={{ color: 'inherit', cursor: 'inherit', border: 0, background: 'none' }}
+                            type="button"
+                        >
+                            <PlusOutlined />
+                            <div style={{ marginTop: 8 }}>Upload</div>
+                        </button>
+                    </Upload>
+                </Form.Item>
 
-export default AddProductPage
+                <Form.Item className='form-item' wrapperCol={{ offset: 6, span: 16 }}>
+                    <Button type="primary" htmlType="submit">
+                        Add Product
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
+    );
+};
+
+export default AddProductPage;
