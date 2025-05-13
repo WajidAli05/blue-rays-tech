d4ecba7b0ed9881c2ed4a8ebbb567e1a64933dd0
WajidAli05 <wajidalise@gmail.com>
2025-05-13 13:44:52 -0700

    Backend scaffolding

---
+++ Backend/config/dbConnection.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async () => {
+    mongoose.connection.on('connected', () => console.log('connected'));
+    mongoose.connection.on('open', () => console.log('open'));
+    mongoose.connection.on('disconnected', () => console.log('disconnected'));
+    mongoose.connection.on('reconnected', () => console.log('reconnected'));
+    mongoose.connection.on('disconnecting', () => console.log('disconnecting'));
+    mongoose.connection.on('close', () => console.log('close'));
+    
+    mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('Database connection established');
+    })
+    .catch((err)=> {
+        console.log('Database connection error: ', err);
+        process.exit(1);
+    })
+}
+
+
+module.exports = dbConnection;
--- Backend/server.js
+++ Backend/server.js
@@ -1,30 +1,15 @@
-// Importing express
-const express = require('express');
+import express, { json } from 'express';
+import { config } from 'dotenv';
+import dbConnection from './config/dbConnection.js';
+
 const app = express();
+config();
+app.use(json());
 
-// Enable Express to parse incoming JSON data
-app.use(express.json());  // This middleware is required to parse JSON in POST requests
+//connect to database
+dbConnection();
 
-// POST route for testing
-app.post('/test', (req, res) => {
-  const { name, email } = req.body;
-
-  // Respond with the received data
-  if (!name || !email) {
-    return res.status(400).json({ message: 'Name and email are required' });
-  }
-
-  res.status(200).json({
-    message: 'Data received successfully',
-    data: {
-      name: name,
-      email: email,
-    },
-  });
-});
-
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
