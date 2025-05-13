142ff5f61caf7f8c106b4e7b94e5c8fd65eb9db6
WajidAli05 <wajidalise@gmail.com>
2025-05-13 13:49:58 -0700

    corrected .gitignore file to ignore node_modules in backend

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,7 @@ app.use(json());
 //connect to database
 dbConnection();
 
+// default port is 3000
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
